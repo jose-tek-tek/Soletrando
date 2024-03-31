774a58e2ccb48069c05ddcfca5823a69fce666b5
Pedrox | Adm! Dev! <163602203+jose-tek-tek@users.noreply.github.com>
2024-03-31 17:28:54 -0300

    Soletrando as soletragens

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Soletrando");
+
+Console.WriteLine();
+
+Console.WriteLine("Digite um texto a ser soletrado");
+
+Console.WriteLine();
+
+string texto = Console.ReadLine()!;
+
+Console.WriteLine("\nSoletrado fica:\n");
+
+Console.WriteLine(string.Join("-",texto.ToUpper().ToCharArray()));
+
+Console.WriteLine();
